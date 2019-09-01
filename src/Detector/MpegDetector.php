a12a02c56649db2a0f52554a044add934d0d70b5
odan <d.opitz@outlook.com>
2019-09-01 23:02:41 +0200

    Optimize MPEG detector

--- src/Detector/MpegDetector.php
+++ src/Detector/MpegDetector.php
@@ -23,7 +23,7 @@ final class MpegDetector implements VideoDetectorInterface
     {
         $bytes = (string)$file->fread(3);
         $binary = (string)$file->fread(1);
-        $number = hexdec(bin2hex($binary));
+        $number = ord($binary);
 
         return $bytes === "\0\0\01" && $number >= 0xb0 && $number <= 0xbf ? new VideoType(
             VideoFormat::MPEG,
