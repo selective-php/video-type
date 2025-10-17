0d133e05f5b959bbed415fdbfe70173efd2feefd
heinrichschiller <heinrich@heinrich-schiller.de>
2025-10-17 14:38:36 +0200

    updating config for "showing less problems"

--- .cs.php
+++ .cs.php
@@ -1,20 +1,25 @@
 <?php
 
-return (new PhpCsFixer\Config())
+use PhpCsFixer\Config;
+
+return (new Config())
   ->setUsingCache(false)
   ->setRiskyAllowed(true)
-  //->setCacheFile(__DIR__ . '/.php_cs.cache')
   ->setRules([
     '@PSR1' => true,
     '@PSR2' => true,
     '@Symfony' => true,
     'psr_autoloading' => true,
-    'yoda_style' => false,
+    'yoda_style' => [
+        'equal' => false,
+        'identical' => false,
+        'less_and_greater' => false
+    ],
     'array_syntax' => ['syntax' => 'short'],
     'list_syntax' => ['syntax' => 'short'],
     'concat_space' => ['spacing' => 'one'],
     'cast_spaces' => ['space' => 'none'],
-    'compact_nullable_typehint' => true,
+    'compact_nullable_type_declaration' => true,
     'increment_style' => ['style' => 'post'],
     'declare_equal_normalize' => ['space' => 'single'],
     'echo_tag_syntax' => ['format' => 'long'],
@@ -22,7 +27,10 @@ return (new PhpCsFixer\Config())
     'phpdoc_align' => false,
     'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
     'phpdoc_order' => true, // psr-5
+    'phpdoc_no_useless_inheritdoc' => false,
     'phpdoc_no_empty_return' => false,
+    'phpdoc_to_comment' => false,
+    'no_superfluous_phpdoc_tags' => false,
     'align_multiline_comment' => true, // psr-5
     'general_phpdoc_annotation_remove' => [
       'annotations' => [
@@ -30,10 +38,16 @@ return (new PhpCsFixer\Config())
         'package',
       ],
     ],
+    'global_namespace_import' => [
+        'import_classes' => true,
+        'import_constants' => null,
+        'import_functions' => null
+    ],
   ])
   ->setFinder(PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/tests')
     ->name('*.php')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true));
+    ->ignoreVCS(true)
+);
